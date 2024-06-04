43228acfce2ad7137eabd5588c75b9c76e2c9dc6
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-06-05 02:37:40 +0700

    working on read list of car

---
+++ src/services/car/index.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:1100/',
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+
+export const getCars = async (endpoint) => {
+  try {
+    const response = await apiClient.get(endpoint);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+export const createCar = async (endpoint, data) => {
+  try {
+    const response = await apiClient.post(endpoint, data);
+    return response.data;
+  } catch (error) {
+    console.error('Error sending data:', error);
+    throw error;
+  }
+};
+
+export const updateCar = async (endpoint, id, data) => {
+  try {
+    const response = await apiClient.put(endpoint, id, data);
+    return response.data;
+  } catch (error) {
+    console.error('Error sending data:', error);
+    throw error;
+  }
+};
+
+export const deleteCar = async (endpoint, id) => {
+  try {
+    const response = await apiClient.delete(endpoint, id);
+    return response.data;
+  } catch (error) {
+    console.error('Error sending data:', error);
+    throw error;
+  }
+};
+
+export default apiClient;
--- src/views/car/index.js
+++ src/views/car/index.js
@@ -1,7 +1,36 @@
-import React from 'react';
-import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Image, SimpleGrid, Stack, Text } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text
+} from '@chakra-ui/react';
+import { getCars } from '../../services/car';
 
 export default function CarView() {
+  const [ cars, setCars ] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getCars('/cars');
+        console.log('data', data);
+        setCars(data);
+      } catch (error) {
+        throw new Error();
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return(
     <Box mt="100px" pl="20px">
@@ -9,7 +38,40 @@ export default function CarView() {
         columns={{ sm: 2, md: 3 }}
         gap='10px'
       >
-        <Card maxW='sm' mb='40px'>
+        {
+          cars.map((data) => {
+            return (
+              <div key={data.car_id}>
+                <Card maxW='sm' mb='40px'>
+                  <CardBody>
+                    <Image
+                      src={data.image}
+                      alt='Green double couch with wooden legs'
+                      borderRadius='lg'
+                    />
+                    <Stack mt='6' spacing='3'>
+                      <Heading size='md'>
+                        <span>{`${data.car_name} - ${data.car_model}`}</span>
+                      </Heading>
+                      <Text>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tincidunt nec ligula sed tristique. Vivamus sit amet nulla sit amet libero euismod malesuada quis luctus orci.
+                      </Text>
+                    </Stack>
+                  </CardBody>
+                  <Divider />
+                  <CardFooter>
+                    <ButtonGroup spacing='2'>
+                      <Button variant='solid' colorScheme='blue'>
+                        Rent Now
+                      </Button>
+                    </ButtonGroup>
+                  </CardFooter>
+                </Card>
+              </div>
+            )
+          })
+        }
+        {/* <Card maxW='sm' mb='40px'>
           <CardBody>
             <Image
               src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
@@ -158,32 +220,7 @@ export default function CarView() {
               </Button>
             </ButtonGroup>
           </CardFooter>
-        </Card>
-        <Card maxW='sm' mb='40px'>
-          <CardBody>
-            <Image
-              src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-              alt='Green double couch with wooden legs'
-              borderRadius='lg'
-            />
-            <Stack mt='6' spacing='3'>
-              <Heading size='md'>Living room Sofa</Heading>
-              <Text>
-                This sofa is perfect for modern tropical spaces, baroque inspired
-                spaces, earthy toned spaces and for people who love a chic design with a
-                sprinkle of vintage design.
-              </Text>
-            </Stack>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <ButtonGroup spacing='2'>
-              <Button variant='solid' colorScheme='blue'>
-                Buy now
-              </Button>
-            </ButtonGroup>
-          </CardFooter>
-        </Card>
+        </Card> */}
       </SimpleGrid>
     </Box>
   )
